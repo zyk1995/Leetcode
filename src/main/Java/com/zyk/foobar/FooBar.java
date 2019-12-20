174b6b6f98a10a9c51f32a9503a1da7d16bf5521
zyk1995 <1251134350@qq.com>
2019-12-20 18:06:10 +0800

    [add][zyk]foobar

---
+++ src/main/Java/com/zyk/foobar/FooBar.java
@@ -0,0 +1,52 @@
+package com.zyk.foobar;
+
+import com.zyk.printSorted.Solution;
+
+public class FooBar {
+    private int n;
+    private volatile boolean flag = false;
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while (flag) {
+                Thread.yield();
+            }
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            flag = true;
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while (!flag) {
+                Thread.yield();
+            }
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            flag = false;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        FooBar fooBar = new FooBar(10);
+
+        fooBar.foo(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("foo");
+            }
+        });
+        fooBar.bar(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("bar");
+            }
+        });
+    }
+}
